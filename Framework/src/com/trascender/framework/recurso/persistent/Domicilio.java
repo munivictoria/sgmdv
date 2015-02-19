8b56e0347a61d60911eb7f60b29e2fe777b73afb
ferna <ferna@localhost>
2015-02-19 14:09:58 -0300

    Publicar metodo de armado de domicilio

--- Framework/src/com/trascender/framework/recurso/persistent/Domicilio.java
+++ Framework/src/com/trascender/framework/recurso/persistent/Domicilio.java
@@ -259,6 +259,10 @@ public class Domicilio implements Serializable, Cloneable{
 	@PrePersist
 	@PreUpdate
 	public void beforeSaveUpdate() {
+		this.armarDomicilio();
+	}
+	
+	public void armarDomicilio() {
 		this.domicilioArmado = this.toString();
 	}
 
