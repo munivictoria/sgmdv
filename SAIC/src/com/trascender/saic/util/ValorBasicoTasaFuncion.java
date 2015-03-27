39271f2efd6dfd59dea9a7a186bdc324f5e5f661
ferna <ferna@localhost>
2015-03-27 12:30:37 -0300

    Funcion para recuperar el valor basico de la tasa durante la liquidacion

---
+++ SAIC/src/com/trascender/saic/util/ValorBasicoTasaFuncion.java
@@ -0,0 +1,26 @@
+package com.trascender.saic.util;
+
+import java.util.Stack;
+
+import org.nfunk.jep.ParseException;
+import org.nfunk.jep.function.PostfixMathCommand;
+
+import com.trascender.saic.recurso.persistent.LiquidacionTasa;
+import com.trascender.saic.recurso.persistent.ModificadorLiquidacion;
+
+public class ValorBasicoTasaFuncion extends PostfixMathCommand {
+	
+	public static String VALOR_BASICO_TASA = "VALOR_BASICO_TASA";
+
+	private LiquidacionTasa liquidacionTasa;
+	
+	public ValorBasicoTasaFuncion(LiquidacionTasa pLiquidacion) {
+		numberOfParameters = 0;
+		this.liquidacionTasa = pLiquidacion;
+	}
+
+	public void run(Stack stack) throws ParseException {
+		checkStack(stack);
+		stack.push(liquidacionTasa.getValor());
+	}
+}
